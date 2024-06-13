b42ab45bba6401e14903f13252dc542682f6ac7c
LadyTano <sarah.tannert@gmail.com>
2024-06-12 22:23:42 -0400

    added endpoit for filtering books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,8 +30,9 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let filtered_books = Object.filter((books) => books.title === title);
+    res.send(filtered_books);
 });
 
 //  Get book review
